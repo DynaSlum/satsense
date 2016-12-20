7df7baab7baff6b4bf515b9fbf0eeafc34f56a88
elboyran <e.ranguelova@esciencecenter.nl>
2016-12-20 14:13:06 +0100

    issue #5: the unit test is also a script to create 5 data stores.

--- MATLAB/ImageTiling/unit_tests/test_createImageDatastore.m
+++ MATLAB/ImageTiling/unit_tests/test_createImageDatastore.m
@@ -2,11 +2,30 @@
 
 %% parameters
 base_path = 'C:\Projects\DynaSlum\Data\Kalyan\Datasets\';
-image_dataset_location = fullfile(base_path,'px417m250');
-summary_flag = true;
-preview_flag = true;
+tile_sizes = [417 333 250 167 83];
+tile_sizes_m = [250 200 150 100 50];
 
+num_datasets = length(tile_sizes);
+
+summary_flag = true;
+preview_flag = false;
+
+save_flag = true;
 
 %% create image datastore and show summary and sample of the 4 classes
-[imds] = createImageDatastore( image_dataset_location, summary_flag,...
-    preview_flag);
+for n = 1: num_datasets
+    disp(['Creating image data store # ', num2str(n), ' out of ', ...
+        num2str(num_datasets)]);
+    tile_size = tile_sizes(n);
+    tile_size_m = tile_sizes_m(n);
+    str = ['px' num2str(tile_size) 'm' num2str(tile_size_m)];
+    image_dataset_location = fullfile(base_path,str);
+    [imds] = createImageDatastore( image_dataset_location, summary_flag,...
+        preview_flag);
+    if save_flag
+        sav_file = fullfile(image_dataset_location, 'imds.mat');
+        save(sav_file, 'imds');
+    end
+    disp('-----------------------------------------------------------------');
+end
+disp('DONE.');
