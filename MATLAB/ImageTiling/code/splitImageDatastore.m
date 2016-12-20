57efd319c2981cb3f753318f4ee270d47c3892a7
elboyran <e.ranguelova@esciencecenter.nl>
2016-12-20 17:08:13 +0100

    issue #12: splitting the image datastore into Training, Testing and Validaiton sets.

---
+++ MATLAB/ImageTiling/code/splitImageDatastore.m
@@ -0,0 +1,36 @@
+function [imdsTrain, imdsTest, imdsValidation] = splitImageDatastore( full_imds,...
+    fractionTrain, fractionTest, summary_flag)
+
+
+%% splitImageDatastore  wrapper aroind splitEachLabel for datasets with 
+%   some preset parameters
+%   INPUT:
+%   full_imds -the datastore to be splitin Train, Test and Validation sub-sets
+%   fractionTrain -fractio (between [0 and 1)) for the training set
+%   fractionTest -fraction (between [0 and 1)) for the testing set
+%   Note: what remains is validation, i.e. 
+%   fractionValidation = 1- (fractionTrain +fractionTest)
+%   summary_flag- if true show thr label distribution per sets
+
+%   OUPUT:
+%   imdsTrain/Test/Validation - the split datastores
+% For Testing use test_splitImageDatastore
+% Note: see also https://nl.mathworks.com/help/vision/ug/...
+% image-classification-with-bag-of-visual-words.html
+%% split the full datastore
+[imdsTrain, imdsTest, imdsValidation] = splitEachLabel(full_imds, fractionTrain,...
+    fractionTest,'randomize');
+
+%% Display Class Names and Counts
+if summary_flag  
+    tblTrain = countEachLabel(imdsTrain);                     %#ok
+    disp('Training datastore: ');
+    disp(tblTrain);
+    tblTest = countEachLabel(imdsTest);                     %#ok
+    disp('Testing datastore: ');
+    disp(tblTest);
+    tblValidation = countEachLabel(imdsValidation);                %#ok
+    disp('Validaiton datastore: ');
+    disp(tblValidation);    
+end
+
---
+++ MATLAB/ImageTiling/unit_tests/test_splitImageDatastore.m
@@ -0,0 +1,34 @@
+% Testing splitImageDatastore.m
+
+%% parameters
+base_path = 'C:\Projects\DynaSlum\Data\Kalyan\Datasets\';
+tile_sizes = [417 333 250 167 83];
+tile_sizes_m = [250 200 150 100 50];
+
+fractionTrain = 0.7;
+fractionTest = 0.15;
+num_datasets = length(tile_sizes);
+
+summary_flag = true;
+save_flag = true;
+
+%% split the image datastore and show summary of each subset
+for n = 1: num_datasets
+    disp(['Splitting image data store # ', num2str(n), ' out of ', ...
+        num2str(num_datasets)]);
+    tile_size = tile_sizes(n);
+    tile_size_m = tile_sizes_m(n);
+    str = ['px' num2str(tile_size) 'm' num2str(tile_size_m)];
+    image_dataset_location = fullfile(base_path,str);
+    sav_file = fullfile(image_dataset_location, 'imds.mat');
+    load(sav_file);    
+    
+    [imdsTrain, imdsTest, imdsValidation] = splitImageDatastore(imds,...
+    fractionTrain, fractionTest, summary_flag);
+
+    if save_flag  
+        save(sav_file, 'imdsTrain', 'imdsTest', 'imdsValidation', '-append');
+    end
+    disp('-----------------------------------------------------------------');
+end
+disp('DONE.');
