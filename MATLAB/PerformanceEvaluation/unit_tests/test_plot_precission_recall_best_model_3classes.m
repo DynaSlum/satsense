397286306c3a9e35fcfa7cc0ef1f6b132ab08de8
elboyran <e.ranguelova@esciencecenter.nl>
2017-01-27 17:21:01 +0100

    issue #28: function and script for Precission and Recall plots for the best classifier for all classes

---
+++ MATLAB/PerformanceEvaluation/unit_tests/test_plot_precission_recall_best_model_3classes.m
@@ -0,0 +1,47 @@
+% test_plot_accuracy_models
+
+%% parameters
+if isunix
+    root_dir = fullfile('/home','elena','DynaSlum');
+else
+    root_dir = fullfile('C:','Projects', 'DynaSlum');
+end
+
+base_path = fullfile(root_dir, 'Results','Classification3Classes','PerformanceComparision');
+tile_sizes = [417 333 250 167];
+tile_sizes_m = [250 200 150 100];
+
+num_datasets = length(tile_sizes);
+
+%% display accuracy plots for each dataset
+for n = 1 : num_datasets
+     disp(['Creating the best classifier Precission and Recall plot image training data store # ', num2str(n), ' out of ', ...
+            num2str(num_datasets)])
+    % load the data table containing the performance measures
+    tile_size = tile_sizes(n);
+    tile_size_m = tile_sizes_m(n);
+    res_str = ['px' num2str(tile_size) 'm' num2str(tile_size_m)];
+    str = [num2str(n) res_str];
+    fname = ['ClassifPerfDataset' str 'TrainValTable.mat'];
+    perf_dataset_file = fullfile(base_path,fname); 
+    
+    load(perf_dataset_file);
+    varname = ['ClassifPerfDataset' str 'TrainVal'];
+    data_table = eval(varname);   
+    
+    % plot the accuracy of all models
+    title_str = ['Validation performance best classifier for tiles of size ' res_str];
+    
+    switch n
+        case 1
+            best_class_ind = 21;
+        case 2, 3, 4
+            best_class_ind = 12;
+    end
+            
+    [fig_h] = plot_precission_recall_best_model_3classes( data_table, best_class_ind, ...
+        [0 1 0],[],title_str);
+    
+end
+
+
