8a327a5e4231b64abd56167d9db6f81712aa1820
elboyran <e.ranguelova@esciencecenter.nl>
2017-02-08 13:54:56 +0100

    issue #36: convert BoVW to features tables with different vocabulary sizes in a loop

--- MATLAB/CLassifierTraining/unit_tests/test_createFeatureTable.m
+++ MATLAB/CLassifierTraining/unit_tests/test_createFeatureTable.m
@@ -20,6 +20,8 @@ tile_sizes_m = [80];
 
 num_datasets = length(tile_sizes);
 
+vocabulary_sizes = [10 20 50];
+
 save_flag = true;
 verbose = true;
 
@@ -34,42 +36,46 @@ for n = 1 : num_datasets
     image_dataset_location = fullfile(base_path,str);
     datastore_file = fullfile(image_dataset_location, 'imds.mat');
     
-    if train
-        disp(['Creating Feature Table for image training data store # ', num2str(n), ' out of ', ...
-            num2str(num_datasets)]);
-        load(datastore_file, 'imdsTrain');
-        
-        % load the feature vectors
-        features_file = fullfile(image_dataset_location, 'BoVWTrain.mat');
-        load(features_file, 'feature_vectors');
-        
-        % create table
-        [feature_table] = createFeatureTable( imdsTrain, feature_vectors);
-        
-        %% save
-        if save_flag
-            sav_file = fullfile(image_dataset_location, 'FeatureTableTrain.mat');
-            save(sav_file, 'feature_table');
+    for vocabulary_size = vocabulary_sizes
+        disp(['Vocabulary size: ' num2str(vocabulary_size)]);
+        if train
+            disp(['Creating Feature Table for image training data store # ', num2str(n), ' out of ', ...
+                num2str(num_datasets)]);
+            load(datastore_file, 'imdsTrain');
+            
+            % load the feature vectors
+            features_file = fullfile(image_dataset_location,  ['Bo' num2str(vocabulary_size) 'VWTrain.mat']);
+            load(features_file, 'feature_vectors');
+            
+            % create table
+            [feature_table] = createFeatureTable( imdsTrain, feature_vectors);
+            
+            %% save
+            if save_flag
+                sav_file = fullfile(image_dataset_location, ['Feature' num2str(vocabulary_size) 'TableTrain.mat']);
+                save(sav_file, 'feature_table');
+            end
+        end
+        if test
+            disp(['Creating Feature Table for image testing data store # ', num2str(n), ' out of ', ...
+                num2str(num_datasets)]);
+            load(datastore_file, 'imdsTest');
+            
+            % load the feature vectors
+            features_file = fullfile(image_dataset_location, ['Bo' num2str(vocabulary_size) 'VWTest.mat']);
+            load(features_file, 'feature_vectors');
+            
+            % create table
+            [feature_table] = createFeatureTable( imdsTest, feature_vectors);
+            
+            %% save
+            if save_flag
+                sav_file = fullfile(image_dataset_location, ['Feature' num2str(vocabulary_size) 'TableTest.mat']);
+                save(sav_file, 'feature_table');
+            end
+            
         end
     end
-    if test
-        disp(['Creating Feature Table for image testing data store # ', num2str(n), ' out of ', ...
-            num2str(num_datasets)]);
-        load(datastore_file, 'imdsTest');
-        
-        % load the feature vectors
-        features_file = fullfile(image_dataset_location, 'BoVWTest.mat');
-        load(features_file, 'feature_vectors');
-        
-        % create table
-        [feature_table] = createFeatureTable( imdsTest, feature_vectors);
-        
-        %% save
-        if save_flag
-            sav_file = fullfile(image_dataset_location, 'FeatureTableTest.mat');
-            save(sav_file, 'feature_table');
-        end
-    end    
     
     disp('-----------------------------------------------------------------');
 end
