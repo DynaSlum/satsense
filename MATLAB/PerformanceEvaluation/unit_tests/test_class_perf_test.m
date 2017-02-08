b77c139824ae358647e2f3696afae688d1777ad7
elboyran <e.ranguelova@esciencecenter.nl>
2017-02-08 20:28:44 +0100

    tetsing for multiple vocabulary sizes

--- MATLAB/PerformanceEvaluation/unit_tests/test_class_perf_test.m
+++ MATLAB/PerformanceEvaluation/unit_tests/test_class_perf_test.m
@@ -13,9 +13,11 @@ base_classif_path = fullfile(root_dir, 'Classification3Classes', 'Classifiers');
 % tile_sizes = [417 333 250 167];
 % tile_sizes_m = [250 200 150 100];
 
-tile_sizes = [417 333 250];
-tile_sizes_m = [250 200 150];
-
+% tile_sizes = [417 333 250];
+% tile_sizes_m = [250 200 150];
+vocabulary_sizes = [10 20 50];
+tile_sizes = [100 ];
+tile_sizes_m = [80 ];
 
 num_datasets = length(tile_sizes);
 
@@ -25,16 +27,30 @@ for n = 1 : num_datasets
     tile_size = tile_sizes(n);
     tile_size_m = tile_sizes_m(n);
     str = ['px' num2str(tile_size) 'm' num2str(tile_size_m)];
-    dataset_features_location = fullfile(base_features_path,str, 'FeatureTableTest.mat');
     
-    classifier_location = fullfile(base_classif_path, ['trainedClassifier_' str]);
-    
-    load(dataset_features_location); % contains feature_table
-    load(classifier_location); % contains trainedClassifier
-    
-    disp(['Computing Best classifier performance for image testing data store # ', num2str(n), ' out of ', ...
+    for vocabulary_size = vocabulary_sizes
+        disp(['Vocabulary size: ' num2str(vocabulary_size)]);
+        
+        dataset_features_location = fullfile(base_features_path,str, ['Feature' num2str(vocabulary_size) 'TableTest.mat']);
+        
+        classifier_location = fullfile(base_classif_path, ['trainedClassifier' num2str(vocabulary_size) '_' str]);
+        
+        load(dataset_features_location); % contains feature_table
+        load(classifier_location); % contains trainedClassifier
+        
+        disp(['Computing Best classifier performance for image testing data store # ', num2str(n), ' out of ', ...
             num2str(num_datasets)]);
-    [~,~, perf_stats] = class_perf_test(feature_table, trainedClassifier);
-    disp('Confusion matrix: ');
-    disp(perf_stats.confusionMat);
+        [~,~, perf_stats] = class_perf_test(feature_table, trainedClassifier);
+        disp('Classes: ');
+        disp(perf_stats.groupOrder);
+        disp('Confusion matrix: ');
+        disp(perf_stats.confusionMat);
+        disp('Accuracy, [%]: ');
+        disp(perf_stats.accuracy*100);
+        disp('Recall/TRP/Sensitivity, [%]: ');
+        disp(perf_stats.sensitivity*100);
+        disp('Precision/PPV, [%]: ');
+        disp(perf_stats.precision*100);
+        disp('----------------------------------------------------------------------------------');
+    end
 end
