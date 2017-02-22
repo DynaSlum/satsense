14161a70487098708c4e7e707754742dfc00b926
elboyran <e.ranguelova@esciencecenter.nl>
2017-02-22 17:46:20 +0100

    saving tile to a file now can save also tiles per central pixel, not anly extent and without class label as well

--- MATLAB/DataPreparation/code/saveTile2File.m
+++ MATLAB/DataPreparation/code/saveTile2File.m
@@ -1,7 +1,8 @@
 function [filename] = saveTile2File(image_data,  extent, path, class_label, base_fname, ext)
 %% saveTile2File  saving image tile (patch) to file
 %   image_data - 3D array of image data values
-%   extent - vector of 4 elements, starting and ending row and column indicies
+%   extent - either vector of 4 elements, starting and ending row and
+%            column indicies OR vector with 2 elements- row and column
 %   path - dataset path
 %   class_label- the tile/patch class label. Can be one of "Slum",
 %               "BuiltUp" or "NonBuiltUp"
@@ -10,25 +11,43 @@ function [filename] = saveTile2File(image_data,  extent, path, class_label, base
 % For Testing use test_saveTile2File
 
 %% input control
-if not(ismember(class_label,{'BuiltUp','NonBuiltUp','Slum','Mixed'}))
+if not(ismember(class_label,{'','BuiltUp','NonBuiltUp','Slum','Mixed'}))
     error('saveTile2File: unknown class label!');
 end
 
-if length(extent) ~= 4
-    error('saveTile2File: the tile extent should have 4 elements- starting and ending row and column index!');
+if length(extent) ~= 4 && length(extent) ~= 2
+    error('saveTile2File: the tile extent should have 4 elements- starting and ending row and column index or 2 elements- pixel row and column!');
 end
 
 %% params -> vars
-sr = extent(1);
-er = extent(2);
-sc = extent(3);
-ec = extent(4);
+if length(extent) == 2
+    pixel_wise = true;
+elseif length(extent) == 4
+    pixel_wise = false;
+end
+
+if pixel_wise
+    r = extent(1);
+    c = extent(2);
+else
+    sr = extent(1);
+    er = extent(2);
+    sc = extent(3);
+    ec = extent(4);
+end
 
 %% generate the filename
-fname = [base_fname '_tile_' 'sr' num2str(sr) 'er' num2str(er) ...
-    'sc' num2str(sc) 'ec' num2str(ec) '.' ext];
-% if the sub-dir doesn't exist creat eit
-full_path = fullfile(path, class_label);
+if pixel_wise
+    fname = [base_fname '_tile_' 'r' num2str(r) ...
+        'c' num2str(c) '.' ext];
+    full_path = path;
+else
+    
+    fname = [base_fname '_tile_' 'sr' num2str(sr) 'er' num2str(er) ...
+        'sc' num2str(sc) 'ec' num2str(ec) '.' ext];
+    % if the sub-dir doesn't exist create it
+    full_path = fullfile(path, class_label);
+end
 if ~isdir(full_path)
     mkdir(full_path);
 end
