95d2fe05283359050d9bbf55f0ce6591a5b9a562
elboyran <e.ranguelova@esciencecenter.nl>
2017-02-22 21:21:45 +0100

    issue #39: function and script for classifying each pixel using the pre-trained classifiers on tiles

---
+++ MATLAB/ClassifierPrediction/code/classifyEachPixelFromTile.m
@@ -0,0 +1,75 @@
+function [ segmented_image] = classifyEachPixelFromTile( image_fullfname, tile_size, tile_step, classifier)
+%% classifyEachPixelFromTile  classifies each pixel of the original image based on tiles around the pixels
+%   The function classifies each pixel of a given image using a tile around
+%   it given a pre-trained classifer on tiles
+
+%   image_fullname - the full name of the image to be croped/tiles
+%   tile_size - vector for 2 elements- the number of rows and columns of the tile
+%   tile_step - vector for 2 elements- the step size of rows and columns 
+%   classifier - 
+%    
+%   segmented_image- the segmented image having values 3 for Slum (R), 2
+%   for NonBuiltUp (G) and 1- BuiltUp (B)
+% For Testing use test_classifyEachPixelFromTile
+
+%% input control
+if nargin < 4
+    error('classifyEachPixelFromTile: not enough input arguments!');
+end
+
+%% params -> vars
+% basename and extention for the tiles
+[~,base_fname,ext] = fileparts(image_fullfname);  
+ext = ext(2:end);
+
+
+%% load the data from the input file
+image_data = imread(image_fullfname);
+
+%% dimensions
+nrows = size(image_data, 1);
+ncols = size(image_data, 2);
+
+%% initializations
+segmented_image = zeros(nrows, ncols);
+
+nrows_tile = tile_size(1); ncols_tile = tile_size(2);
+half_nrows_tile = fix(nrows_tile/2);  half_ncols_tile = fix(ncols_tile/2); 
+nrows_step = tile_step(1); ncols_step = tile_step(2);
+
+%% tiling
+for r = 1: nrows_step : nrows
+    
+    sr = max(1, r - half_nrows_tile);
+    er = min(nrows, r + half_nrows_tile - 1);
+    for c = 1: ncols_step : ncols
+       
+        sc = max(1, c - half_ncols_tile);
+        ec = min(ncols, c + half_ncols_tile - 1);
+        disp('Processing pixel :'); pixel = [r c]
+        
+        
+        % get the image tile
+        image_tile = image_data(sr:er, sc:ec, :);                
+        
+        % classify the image using the classifier
+        tic
+        [labelIdx, scores] = predict(classifier, image_tile);
+        toc
+        class_label = char(classifier.Labels(labelIdx))
+        
+        switch class_label
+            case 'Slum'
+                l = 3;
+            case 'NonBuiltUp'
+                l = 2;
+            case 'BuiltUp'
+                l = 1;
+        end
+        
+        segmented_image(r,c) = l;                         
+    end
+end
+
+end
+
---
+++ MATLAB/ClassifierPrediction/unit_tests/test_classifyEachPixelFromTile.m
@@ -0,0 +1,47 @@
+% Testing of classifyEachPixelFromTile
+
+%% parameters
+if isunix
+    root_dir = fullfile('/home','elena','DynaSlum');
+else
+    root_dir = fullfile('C:','Projects', 'DynaSlum');
+end
+
+n = 1;
+tile_sizes = [100];
+tile_sizes_m = [80];
+vocabulary_size = [50];
+tile_size = tile_sizes(n);
+tile_size_m = tile_sizes_m(n);
+stepY = 1;
+stepX = stepY;
+tile_step = [stepX stepY];
+
+data_path = fullfile(root_dir, 'Data','Kalyan','Rasterized_Lourens');
+sav_path = fullfile(root_dir, 'Results', 'Classification3Classes', 'Segmentation');
+image_fname = 'Mumbai_P4_R1C1_3_clipped_rgb.tif';
+str = ['px' num2str(tile_size) 'm' num2str(tile_size_m)];
+sav_path_classifier = fullfile(root_dir, 'Results','Classification3Classes','Classifiers');
+fname = fullfile(sav_path_classifier, ['trained_SURF_SVM_Classifier' num2str(vocabulary_size) '_' str '.mat']) ;
+load(fname); % contains categoryClassifier
+
+image_fullfname = fullfile(data_path, image_fname);
+
+%% classify each pixel
+tic
+[ segmented_image] = classifyEachPixelFromTile( image_fullfname, ...
+    [tile_size tile_size], tile_step, categoryClassifier);
+disp('Done!');
+toc
+%% visualize
+map = [0 0 1; 0 1 0; 1 0 0];
+RGB = ind2rgb(segmented_image,map);
+figure; imshow(RGB); title('Segmented Kalyan cropped image');
+legend({'BuiltUp'; 'NonBuiltUp'; 'Slum'});
+axis on, grid on
+
+%% save
+sav_fname = fullfname(sav_path,['SegmentedImage_SURF_SVM_Classifier' num2str(vocabulary_size) '_' str .mat']);
+save();
+
+disp('DONE!');
