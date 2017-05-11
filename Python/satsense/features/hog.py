7f9ddeefa4f72d64720147f593457ab8f4baf684
Berend Weel <b.weel@esciencecenter.nl>
2017-05-11 15:40:59 +0200

    Added canny edge detection and sift feature source notebooks

--- Python/satsense/features/hog.py
+++ Python/satsense/features/hog.py
@@ -7,12 +7,13 @@ from skimage import data, color, exposure
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-def hog(image, bands=RGB, normalize=True, orientations=8,
-        pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualise=True,
-        transform_sqrt=False):
+def hog(image, bands=RGB, normalize=True, orientations=9,
+        pixels_per_cell=(8, 8), cells_per_block=(3, 3), visualise=True,
+        transform_sqrt=False, feature_vector=True, block_norm='L2-Hys'):
     rgb_image = get_rgb_image(image, bands, normalize=normalize,)
     gray_image = color.rgb2gray(rgb_image)
 
     return skihog(gray_image, orientations=orientations, pixels_per_cell=pixels_per_cell,
                   cells_per_block=cells_per_block, visualise=visualise,
-                  transform_sqrt=transform_sqrt, block_norm='L2-Hys')
+                  transform_sqrt=transform_sqrt, block_norm=block_norm,
+                  feature_vector=feature_vector)
