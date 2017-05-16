215a36fd6fddf060a0c75de4c6225e2036348e7c
elboyran <e.ranguelova@esciencecenter.nl>
2017-05-16 16:19:48 +0200

    issue #42: function for computing Jaccard index between 2 multipolygons; demo notebook- toy example

--- Python/satsense/performance/__init__.py
+++ Python/satsense/performance/__init__.py
@@ -2,5 +2,6 @@
 The initialization module for the performance measures package.
 '''
 from .jaccard_similarity import jaccard_index_binary_masks
+from .jaccard_similarity import jaccard_index_multipolygons
 
-__all__ = ['jaccard_index_binary_masks']
+__all__ = ['jaccard_index_binary_masks', 'jaccard_index_multipolygons']
