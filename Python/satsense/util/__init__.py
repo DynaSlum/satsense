b80108a6d3d880aba02eefc6fd36638084a900eb
elboyran <e.ranguelova@esciencecenter.nl>
2017-07-13 17:48:57 +0200

    issue #57: function to convert mask to multipolygon; notebook toy example: binary mask to shape update

--- Python/satsense/util/__init__.py
+++ Python/satsense/util/__init__.py
@@ -1,12 +1,12 @@
 from .image import load_from_file, save_mask2file, get_rgb_bands, normalize_image, get_grayscale_image, get_ubyte_image
 from .bands import RGB, PLEIADES, WORLDVIEW2, WORLDVIEW3, QUICKBIRD, MONOCHROME
 from .shapefile import show_multipolygon, load_shapefile2multipolygon, save_multipolygon2shapefile
-from .conversions import multipolygon2mask
+from .conversions import multipolygon2mask, mask2multipolygon
 
 __all__ = [
     'RGB', 'PLEIADES', 'WORLDVIEW2', 'WORLDVIEW3', 'QUICKBIRD',
     'MONOCHROME', 'load_from_file', 'save_mask2file','get_rgb_bands', 'normalize_image',
     'show_multipolygon', 'load_shapefile2multipolygon', 'save_multipolygon2shapefile', 
-    'multipolygon2mask',
+    'multipolygon2mask', 'mask2multipolygon',
     'get_grayscale_image','get_ubyte_image'
 ]
--- Python/satsense/util/conversions.py
+++ Python/satsense/util/conversions.py
@@ -9,9 +9,11 @@ Created on Wed Jul 12 17:16:51 2017
 
 # imports
 from rasterio import features
+from rasterio.features import shapes
+from shapely.geometry import shape, MultiPolygon
 
 # conversion from shapely multipolygon to binary mask
-def multipolygon2mask(multipolygon, rows, cols, trans, default_val,fill_val=0, 
+def multipolygon2mask(multipolygon, rows, cols, trans=(1.0, 0.0, 0.0, 0.0, 1.0, 0.0), default_val,fill_val=0, 
                    all_touched=False, dtype=None):
     mask = features.rasterize(
             [multipolygon],
@@ -22,3 +24,19 @@ def multipolygon2mask(multipolygon, rows, cols, trans, default_val,fill_val=0,
             dtype = dtype)
             
     return mask
+    
+# conversion frombinary mask to shapely multipolygon
+def mask2multipolygon(mask_data, mask, trans=(1.0, 0.0, 0.0, 0.0, 1.0, 0.0), conn=4):    
+    geom_results = (
+        {'properties': {'raster_val': v}, 'geometry': s}
+        for i, (s, v) 
+            in enumerate(shapes(mask_data, mask=mask, connectivity = conn, transform=trans)))
+    geometries = list(geom_results)    
+
+    multi = MultiPolygon([shape(geometries[i]['geometry']) for i in range (len(geometries))])
+    
+    if not(multi.is_valid):        
+        print('Not a valid polygon!')
+        
+    return multi    
+        
