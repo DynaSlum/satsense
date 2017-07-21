e49c2137b8c9bac88c7f9d1f3211287f0545d15f
elboyran <e.ranguelova@esciencecenter.nl>
2017-07-21 16:40:42 +0200

    added load_mask_from_file to the __init__.py

--- Python/satsense/util/__init__.py
+++ Python/satsense/util/__init__.py
@@ -1,9 +1,9 @@
 from .shapefile import show_multipolygon, load_shapefile2multipolygon, save_multipolygon2shapefile
 from .conversions import multipolygon2mask, mask2multipolygon
-from .mask import save_mask2file
+from .mask import save_mask2file, load_mask_from_file
 
 __all__ = [
-    'save_mask2file',
+    'save_mask2file', 'load_mask_from_file',
     'show_multipolygon', 'load_shapefile2multipolygon', 'save_multipolygon2shapefile', 
     'multipolygon2mask', 'mask2multipolygon',
 ]
