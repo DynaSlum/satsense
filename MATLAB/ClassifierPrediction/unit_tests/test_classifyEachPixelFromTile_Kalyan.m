d9648766c05f110e7fcfc551b6dae65eb2aa5aaa
elboyran <e.ranguelova@esciencecenter.nl>
2017-10-30 09:51:13 +0100

    issue #64: renaming old script

---
+++ MATLAB/ClassifierPrediction/unit_tests/test_classifyEachPixelFromTile_Kalyan.m
@@ -0,0 +1,49 @@
+% Testing of classifyEachPixelFromTile
+
+%% parameters
+if isunix
+    root_dir = fullfile('/home','elena','DynaSlum');
+else
+    root_dir = fullfile('C:','Projects', 'DynaSlum');
+end
+
+n = 1;
+tile_sizes = [100];
+tile_sizes_m = [80];
+vocabulary_size = [50];
+tile_size = tile_sizes(n);
+tile_size_m = tile_sizes_m(n);
+%stepY = 1;
+stepY = 5;
+stepX = stepY;
+tile_step = [stepX stepY];
+
+data_path = fullfile(root_dir, 'Data','Kalyan','Rasterized_Lourens');
+sav_path = fullfile(root_dir, 'Results', 'Segmentation');
+image_fname = 'Mumbai_P4_R1C1_3_clipped_rgb.tif';
+str = ['px' num2str(tile_size) 'm' num2str(tile_size_m)];
+sav_path_classifier = fullfile(root_dir, 'Results','Classification3Classes','Classifiers');
+fname = fullfile(sav_path_classifier, ['trained_SURF_SVM_Classifier' num2str(vocabulary_size) '_' str '.mat']) ;
+load(fname); % contains categoryClassifier
+
+image_fullfname = fullfile(data_path, image_fname);
+
+%% classify each pixel
+tic
+[ segmented_image] = classifyEachPixelFromTile( image_fullfname, ...
+    [tile_size tile_size], tile_step, categoryClassifier);
+disp('Done!');
+toc
+%% visualize
+map = [0 0 1; 0 1 0; 1 0 0; 1 1 1]; % White, Blue, Green, Red, White = 1,2,3, NaN
+RGB = ind2rgb(segmented_image,map);
+figure; imshow(RGB, map); title('Segmented Kalyan cropped image (every 5th pixel)');
+%legend('Not processed','BuiltUp', 'NonBuiltUp', 'Slum');
+colorbar('Ticks', [0.1 0.35 0.65 0.9], 'TickLabels', {'BuiltUp', 'NonBuiltUp', 'Slum', 'Not Processed'});
+axis on, grid on
+
+%% save
+sav_fname = fullfile(sav_path,['SegmentedImage_SURF_SVM_Classifier' num2str(vocabulary_size) '_' str '.mat']);
+save(sav_fname,'segmented_image');
+
+disp('DONE!');
