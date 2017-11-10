404ef9ad8d219bac674fa994390c2f504de0e64e
elboyran <e.ranguelova@esciencecenter.nl>
2017-11-10 14:28:44 +0100

    issue #64: first ROI zoom annotated areas are implemented

---
+++ MATLAB/tools/show_image_overlaid_labels_Bangalore_zoom.m
@@ -0,0 +1,77 @@
+
+%% params
+[ paths, processing_params, exec_flags] = config_params_Bangalore();
+
+[data_dir, masks_dir, ~, ~] = v2struct(paths);
+[~, ~, ~, ~, ~, ~, ROIs] = v2struct(processing_params);
+[verbose, visualize, sav] = v2struct(exec_flags);
+
+num_ROIs = length(ROIs);
+
+%% displaying
+for r = 1%:num_ROIs
+    roi = ROIs{r};
+    
+    if verbose
+        disp(['Displaying ROI: ', roi, '...']);
+    end
+    
+    image_data = imread(fullfile(data_dir,['Bangalore_' roi '.tif']));
+    
+    nrows = size(image_data,1);
+    ncols = size(image_data,2);
+    
+    %% load the mask data
+    slum_mask = imread(fullfile(masks_dir,['Bangalore_' roi '_slumMask.tif']));
+    slum_mask = slum_mask * 255;
+    builtup_mask = imread(fullfile(masks_dir,['Bangalore_' roi '_urbanMask.tif']));
+    % builtup_mask = builtup_mask * 255;
+    nonbuiltup_mask = imread(fullfile(masks_dir,['Bangalore_' roi '_vegetationMask.tif']));
+    % nonbuiltup_mask = nonbuiltup_mask * 255;
+    %% prepare colored overlays
+    red = cat(3, ones(nrows,ncols), zeros(nrows, ncols), zeros(nrows,ncols));
+    green = cat(3, zeros(nrows,ncols), ones(nrows,ncols), zeros(nrows,ncols));
+    blue = cat(3, zeros(nrows,ncols), zeros(nrows,ncols), ones(nrows,ncols));
+    
+    %     %% display the image daya
+    %     figure;imshow(image_data);
+    %     axis on, grid on;
+    %     title('Image data');
+    %
+    %% add special rectangles of interest
+    switch r
+        case 1
+            rec_pos_g = [3534 4036 446 329; 4694 4005 255 131;...
+                4243 4518 243 423; 4527 4488 427 263;
+                3312 4685 115 123];
+            labels_str = {'GT1'; 'GT2'; 'GT3'; 'GT4'; 'GT5'};
+            image_data = insertObjectAnnotation(image_data,'Rectangle',rec_pos_g,...
+                        labels_str, 'Font', 'LucidaTypewriterBold', 'FontSize',42, ...
+                        'TextColor','black', 'Color', 'green', 'TextBoxOpacity',0.7,'LineWidth',12);
+            rec_pos_r = [3801 5023 108 71; 4886 5051 87 47];
+            labels_str = {'GT6';'GT7'};
+            image_data = insertObjectAnnotation(image_data,'Rectangle',rec_pos_r,...
+            labels_str, 'Font', 'LucidaTypewriterBold','FontSize',36,'TextColor','white',...
+                        'Color', 'red', 'TextBoxOpacity',0.7,'LineWidth',6);
+            
+        otherwise
+            error('Unsupported ROI');
+    end
+    %% display the image daya and overlap the overlays
+    figure; imshow(image_data);
+    hold on;
+    hr=imshow(red);
+    set(hr, 'AlphaData', 0.2*slum_mask);
+    hg=imshow(green);
+    set(hg, 'AlphaData', 0.2*nonbuiltup_mask);
+    hb=imshow(blue);
+    set(hb, 'AlphaData', 0.2*builtup_mask);
+    hold off
+    map = [0 0 1; 0 1 0; 1 0 0]; % Blue, Green, Red = 1,2,3
+    
+    axis on, grid on;
+    %title('Ground truth overalyed on Kalyan cropped image');
+    colormap(map);
+    colorbar('Ticks', [0.2 0.5 0.8], 'TickLabels', {'BuiltUp', 'NonBuiltUp', 'Slum'});
+    axis([3000 ncols 3900 nrows]);
+end
---
+++ MATLAB/tools/show_image_overlaid_segmentation_Bangalore_zoom.m
@@ -0,0 +1,158 @@
+% show_image_overlaid_segmentation - generating a figure with overlaid
+% segmentation on it
+
+
+%% params
+[ paths, processing_params, exec_flags] = config_params_Bangalore();
+
+[data_dir, masks_dir, ~, segmentation_dir] = v2struct(paths);
+[vocabulary_size, best_tile_size, best_tile_size_m, ~, ~, ~, ROIs] = v2struct(processing_params);
+[verbose, visualize, sav] = v2struct(exec_flags);
+
+str = ['px' num2str(best_tile_size) 'm' num2str(best_tile_size_m)];
+
+num_ROIs = length(ROIs);
+vis_initial_result = false;
+vis_filled_result = false;
+vis_final_result = true;
+
+overlay = true;
+result_only = false;
+mapw = [0 0 1; 0 1 0; 1 0 0; 1 1 1]; % Blue, Green, Red, White = 1,2,3, NaN
+map = [0 0 1; 0 1 0; 1 0 0]; % Blue, Green, Red = 1,2,3
+
+for r = 1%:num_ROIs
+    roi = ROIs{r};
+    
+    if verbose
+        disp(['Displaying ROI: ', roi, '...']);
+    end
+    image_fname = ['Bangalore_' roi '.tif'];
+    image_fullfname = fullfile(data_dir, image_fname);
+    segm_fname = fullfile(segmentation_dir, ['SegmentedImage_SURF_SVM_Classifier' num2str(vocabulary_size) '_' str '_' roi '.mat']);
+    
+    %% load the data and the result
+    image_data = imread(image_fullfname);
+    [nrows, ncols, ~] = size(image_data);
+    load(segm_fname); % contains segmented_image (every 10th pixel); filled_segm_image and segmented_image_denoised
+    
+    
+    %% visualize initial result
+    if vis_initial_result
+        if result_only
+            RGB1 = ind2rgb(segmented_image, mapw);
+            figure; imshow(RGB1, mapw); %title('Segmented Bangalore cropped image');
+            %xlabel('Every 10th pixel is processed');
+            colorbar('Ticks', [0.1 0.35 0.65 0.9], 'TickLabels', {'BuiltUp', 'NonBuiltUp', 'Slum', 'Not Processed'});
+            axis on, grid on
+        end
+        if overlay
+            %% make 3 masks from the multiclass mask
+            [masks] = multiclass_mask2oneclass_masks(segmented_image);
+            red = cat(3, ones(nrows,ncols), zeros(nrows, ncols), zeros(nrows,ncols));
+            green = cat(3, zeros(nrows,ncols), ones(nrows,ncols), zeros(nrows,ncols));
+            blue = cat(3, zeros(nrows,ncols), zeros(nrows,ncols), ones(nrows,ncols));
+            
+            figure; imshow(image_data);
+            hold on;
+            hr=imshow(red);
+            set(hr, 'AlphaData', 0.2*masks(:,:,3));
+            hg=imshow(green);
+            set(hg, 'AlphaData', 0.2*masks(:,:,2));
+            hb=imshow(blue);
+            set(hb, 'AlphaData', 0.2*masks(:,:,1));
+            hold off
+            axis on, grid on;
+            % title('Segmentation overlaid on Kalyan cropped image');
+            colormap(mapw);
+            
+        end
+    end
+    
+    %% filled result
+    if vis_filled_result
+        if result_only
+            RGB2 = ind2rgb(filled_segm_image, map);
+            figure; imshow(RGB2, map); %title('Filled segmented Kalyan cropped image');
+            %xlabel('Missing pixels filled with majority vote fom a 20x20 window');
+            colorbar('Ticks', [0.2 0.5 0.8], 'TickLabels', {'BuiltUp', 'NonBuiltUp', 'Slum'});
+            axis on, grid on
+        end
+        if overlay
+            %% make 3 masks from the multiclass mask
+            [masks] = multiclass_mask2oneclass_masks(filled_segm_image);
+            red = cat(3, ones(nrows,ncols), zeros(nrows, ncols), zeros(nrows,ncols));
+            green = cat(3, zeros(nrows,ncols), ones(nrows,ncols), zeros(nrows,ncols));
+            blue = cat(3, zeros(nrows,ncols), zeros(nrows,ncols), ones(nrows,ncols));
+            
+            figure; imshow(image_data);
+            hold on;
+            hr=imshow(red);
+            set(hr, 'AlphaData', 0.2*masks(:,:,3));
+            hg=imshow(green);
+            set(hg, 'AlphaData', 0.2*masks(:,:,2));
+            hb=imshow(blue);
+            set(hb, 'AlphaData', 0.2*masks(:,:,1));
+            hold off
+            axis on, grid on;
+            % title('Filled segmentation overlaid on Kalyan cropped image');
+            colormap(map);
+            colorbar('Ticks', [0.2 0.5 0.8], 'TickLabels', {'BuiltUp', 'NonBuiltUp', 'Slum'});
+            
+        end
+    end
+    
+    %% final result
+    if vis_final_result
+        if result_only
+            RGB3 = ind2rgb(segmented_image_denoised, map);
+            figure; imshow(RGB3, map); %title('Denoised segmented Kalyan cropped image');
+            %xlabel('Majority filter of size 40x40 is used');
+            colorbar('Ticks', [0.2 0.5 0.8], 'TickLabels', {'BuiltUp', 'NonBuiltUp', 'Slum'});
+            axis on, grid on
+        end
+        if overlay
+            %% add special rectangles of interest
+            switch r
+                case 1
+                    rec_pos_g = [5210 4796 79 194; 4932 4184 64 118;...
+                                 4188 4432 77 63; 3158 4841 170 249;
+                                 3171 4249 98 93];
+                    labels_str = {'S1'; 'S2'; 'S3'; 'S4'; 'S5'};
+                    image_data = insertObjectAnnotation(image_data,'Rectangle',rec_pos_g,...
+                        labels_str, 'Font', 'LucidaTypewriterBold', 'FontSize',42, ...
+                        'TextColor','black', 'Color', 'green', 'TextBoxOpacity',0.7,'LineWidth',12);
+                    rec_pos_b = [4543 4769 150 132];
+                    labels_str = {'S6'};
+                    image_data = insertObjectAnnotation(image_data,'Rectangle',rec_pos_b,...
+                        labels_str, 'Font', 'LucidaTypewriterBold','FontSize',42,'TextColor','white',...
+                        'Color', 'blue', 'TextBoxOpacity',0.7,'LineWidth',12);
+                    
+                otherwise
+                    error('Unsupported ROI');
+            end
+            
+            %% make 3 masks from the multiclass mask
+            [masks] = multiclass_mask2oneclass_masks(segmented_image_denoised);
+            red = cat(3, ones(nrows,ncols), zeros(nrows, ncols), zeros(nrows,ncols));
+            green = cat(3, zeros(nrows,ncols), ones(nrows,ncols), zeros(nrows,ncols));
+            blue = cat(3, zeros(nrows,ncols), zeros(nrows,ncols), ones(nrows,ncols));
+            
+            figure; imshow(image_data);
+            hold on;
+            hr=imshow(red);
+            set(hr, 'AlphaData', 0.2*masks(:,:,3));
+            hg=imshow(green);
+            set(hg, 'AlphaData', 0.2*masks(:,:,2));
+            hb=imshow(blue);
+            set(hb, 'AlphaData', 0.2*masks(:,:,1));
+            hold off
+            axis on, grid on;
+            % title('Denoised segmentation overlaid on Kalyan cropped image');
+            colormap(map);
+            colorbar('Ticks', [0.2 0.5 0.8], 'TickLabels', {'BuiltUp', 'NonBuiltUp', 'Slum'});
+            axis([3000 ncols 3900 nrows]); 
+        end
+    end
+end
+
