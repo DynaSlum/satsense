fad042ab89a75bea6eb3e29ed40b3d31f1da574f
elboyran <e.ranguelova@esciencecenter.nl>
2017-12-08 15:12:16 +0100

    issue #70: script to generate datastores, train and evaluate classifiers

--- MATLAB/CLassifierTraining/simpleImageCategoryClassifier_Kalyan.m
+++ MATLAB/CLassifierTraining/simpleImageCategoryClassifier_Kalyan.m
@@ -1,70 +1,91 @@
 % simplest default training and testing of an Image category classifier
 % using SURF detector and descriptor on Bag of Visual Words (BoVW)
-% classifiation framework and multiclass SVM classifier
+% classifiation framework and multiclass SVM classifier on the ROI from
+% the RGB image of Kalyan
 
-%% setup parameters
-if isunix
-    root_dir = fullfile('/home','bweel','Documents','projects','dynaslum', 'SatelliteImaging','MATLAB');
-else
-    root_dir = fullfile('C:','Projects', 'DynaSlum');
-end
+%% parameters
+[ paths, processing_params, exec_flags] = config_params_Kalyan();
 
-data_dir = '/home/bweel/Documents/projects/dynaslum/data/Bangalore/GE_Images/';
-roi = 'ROI5';
-base_path = fullfile(data_dir, roi);
-sav_path_datastores = fullfile(data_dir, 'Results','Classification3Classes','DatastoresAndFeatures');
-sav_path_classifier = fullfile(data_dir, 'Results','Classification3Classes','Classifiers');
+[data_dir, masks_dir, datastores_dir, classifier_dir, performance_dir,~] = v2struct(paths);
+[~, ~, ~, ~, ~, ~, roi] = v2struct(processing_params);
+[verbose, visualize, sav] = v2struct(exec_flags);
 
-tile_sizes = [135];
-tile_sizes_m = [20];
-%vocabulary_sizes = [10 20 50];
-vocabulary_sizes = [50];
-n = 1;
-%num_datasets = length(tile_sizes);
-tile_size = tile_sizes(n);
-tile_size_m = tile_sizes_m(n);
-str = ['px' num2str(tile_size) 'm' num2str(tile_size_m)];
-%vocabulary_size = vocabulary_sizes(n);
+base_tiles_path = fullfile(data_dir, 'Datasets4MATLAB');
+tile_sizes_m = [50 100 150 200];
+tile_sizes = [84 167 250 334];
+    
+num_datasets = length(tile_sizes);
 
-fractionTrain = 0.3;
+
+%% feature parameters
+
+vocabulary_sizes = [10 20 50];
+
+fractionTrain = 0.8;
 fractionStrongestFeatures = 0.8;
 
-%point_selections = {'Detector','Grid'};
-point_selections = {'Detector'};
+point_selection = {'Detector'};
 
 surf_upright = false;
 
+
+%% execution flags
 summary_flag = true;
-preview_flag = true; % preview of the datastore
-verbose = true;
-visualize = false; % visualization still doesn't work!
-sav = true;
-%% create image datastore
+preview_flag = false; % preview of the datastore
 
-disp(roi);
-disp(['Creating image data store for tile size: ' num2str(tile_size) ' pixels = ' num2str(tile_size_m) ' meters.']);
+if sav
+    if not(exist(datastores_dir,'dir')==7)
+        mkdir(datastores_dir);
+    end
+    
+    if not(exist(classifier_dir,'dir')==7)
+        mkdir(classifier_dir);
+    end
+    
+    if not(exist(performance_dir,'dir')==7)
+        mkdir(performance_dir);
+    end
+end
 
-image_dataset_location = fullfile(base_path,str);
-[imds] = createImageDatastore( image_dataset_location, summary_flag, preview_flag);
-disp('-----------------------------------------------------------------');
-
-%% balance the dataset
-disp('Balancing the datasets.');
-tbl = countEachLabel(imds);
-minSetCount = min(tbl{:,2});
-imds = splitEachLabel(imds, minSetCount, 'randomize');
-countEachLabel(imds)
-
-%% split image datastore
-disp(['Splititng the datastore into Train and Test datastores with fractionTrain: ' num2str(fractionTrain*100) '%']);
-[imdsTrain, imdsTest] = splitImageDatastore(imds, fractionTrain, summary_flag);
-disp('-----------------------------------------------------------------');
-
-%% loop over parameters
-for point_selection = point_selections
+%% create datastores, train and test classifier
+for d = 1: num_datasets
+    %% create image datastores
+    tile_size = tile_sizes(d);
+    tile_size_m = tile_sizes_m(d);
+    str = ['px' num2str(tile_size) 'm' num2str(tile_size_m)];
+    
+    disp(['Creating datastore for dataset ' num2str(d) ':' str '...']);
+    
+     
+    tiles_path = fullfile(base_tiles_path, str, roi);
+    image_dataset_location = tiles_path;
+    
+    [imds] = createImageDatastore( image_dataset_location, summary_flag, preview_flag);
+    
+    %% balance the dataset
+   % disp('Balancing the datasets.');
+    tbl = countEachLabel(imds);
+    minSetCount = min(tbl{:,2});
+    imds = splitEachLabel(imds, minSetCount, 'randomize');
+    countEachLabel(imds)
+    
+    if sav
+        fname = fullfile(datastores_dir, ['datastore_' str '.mat']) ;
+        save(fname, 'imds');
+    end
+    
+    disp('-----------------------------------------------------------------');
+    
+    
+    
+    %% split image datastore
+    disp(['Splititng the datastore into Train and Test datastores with fractionTrain: ' num2str(fractionTrain*100) '%']);
+    [imdsTrain, imdsTest] = splitImageDatastore(imds, fractionTrain, summary_flag);
+    disp('-----------------------------------------------------------------');
+    
+    %% loop over parameters
     for vocabulary_size = vocabulary_sizes
-        %% Create Bag of Visual words
-        
+        %% Create Bag of Visual words        
         disp(['Creating features (BoVW) using vocabulary size ' num2str(vocabulary_size)...
             ' and SUFR point locations ', char(point_selection) ...
             ' for the Train datastore keeping the ' ...
@@ -92,6 +113,13 @@ for point_selection = point_selections
             'VariableNames', {'accuracy';'sensitivity'; 'specificity';...
             'precision';'recall';'Fscore'});
         disp(TrT);
+        
+        % save the performance results
+        if sav
+            disp('Evaluating perfomance on the Training set');
+            fname = fullfile(performance_dir, ['performance_train_' num2str(vocabulary_size) '_' str '.mat']) ;
+            save(fname, 'confmatTrain', 'TrT');
+        end
         disp('-----------------------------------------------------------------');
         disp('Evaluating perfomance on the Test set');
         [confmatTest] = evaluate(categoryClassifier, imdsTest);
@@ -103,12 +131,22 @@ for point_selection = point_selections
             'VariableNames', {'accuracy';'sensitivity'; 'specificity';...
             'precision';'recall';'Fscore'});
         disp(TrTs);
+        % save the performance results
+        if sav
+            disp('Saving perfomance on the Test set');
+            fname = fullfile(performance_dir, ['performance_test_' num2str(vocabulary_size) '_' str '.mat']) ;
+            save(fname, 'confmatTest', 'TrTs');
+        end
         disp('-----------------------------------------------------------------');
         % save the trained classifier
         if sav
-            fname = fullfile(sav_path_classifier, ['trained_SURF_SVM_Classifier' num2str(vocabulary_size) '_' roi '_' str '.mat']) ;
+            fname = fullfile(classifier_dir, ['trained_SURF_SVM_Classifier_' num2str(vocabulary_size) '_' str '.mat']) ;
             save(fname, 'categoryClassifier');
         end
-    end
-end
 
+    end % vocabulary sizes
+    disp('**********************************************************************************')
+    disp('Paused. Press any key to continue');
+    %pause;   
+end % for num_datasets
+    
