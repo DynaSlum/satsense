ea1f9eea460366869eb76bab117352d844eb1924
Bouwe Andela <b.andela@esciencecenter.nl>
2018-05-28 11:46:12 +0200

    Remove unused imports and autoformat

--- satsense/features/sift.py
+++ satsense/features/sift.py
@@ -1,14 +1,17 @@
-import numpy as np
-import scipy as sp
-import cv2
-from satsense import SatelliteImage
-from .feature import Feature
-from sklearn.cluster import KMeans, MiniBatchKMeans
-from sklearn.preprocessing import MinMaxScaler
-from numba import jit
 from typing import Iterator
 
-def sift_cluster(sat_images: Iterator[SatelliteImage], n_clusters=32, sample_size=100000) -> MiniBatchKMeans:
+import cv2
+import numpy as np
+from sklearn.cluster import MiniBatchKMeans
+
+from satsense import SatelliteImage
+
+from .feature import Feature
+
+
+def sift_cluster(sat_images: Iterator[SatelliteImage],
+                 n_clusters=32,
+                 sample_size=100000) -> MiniBatchKMeans:
     sift = cv2.xfeatures2d.SIFT_create()
     base_descriptors = None
     for sat_image in sat_images:
@@ -21,10 +24,10 @@ def sift_cluster(sat_images: Iterator[SatelliteImage], n_clusters=32, sample_siz
         else:
             base_descriptors = np.append(base_descriptors, descriptors, axis=0)
 
-
     # Sample {sample_size} descriptors from all descriptors
     # (Takes random rows) and cluster these
-    X = base_descriptors[np.random.choice(base_descriptors.shape[0], sample_size, replace=False), :]
+    X = base_descriptors[np.random.choice(
+        base_descriptors.shape[0], sample_size, replace=False), :]
 
     mbkmeans = MiniBatchKMeans(n_clusters=n_clusters, random_state=42).fit(X)
 
@@ -32,7 +35,10 @@ def sift_cluster(sat_images: Iterator[SatelliteImage], n_clusters=32, sample_siz
 
 
 class Sift(Feature):
-    def __init__(self, kmeans: MiniBatchKMeans, windows=((25, 25),), normalized=True):
+    def __init__(self,
+                 kmeans: MiniBatchKMeans,
+                 windows=((25, 25), ),
+                 normalized=True):
         super(Sift, self)
         self.windows = windows
         self.kmeans = kmeans
@@ -47,22 +53,21 @@ class Sift(Feature):
             win = cell.super_cell(window, padding=True)
             start_index = i * n_clusters
             end_index = (i + 1) * n_clusters
-            result[start_index: end_index] = self.sift(win.gray_ubyte, self.kmeans)
+            result[start_index:end_index] = self.sift(win.gray_ubyte,
+                                                      self.kmeans)
         return result
 
     def __str__(self):
         normalized = "normalized" if self.normalized == True else "not-normalized"
         return "Sift-{}-{}".format(str(self.windows), normalized)
 
-
     def sift(self, window_gray_ubyte, kmeans: MiniBatchKMeans):
         """
         Calculate the sift feature on the given window
         """
-        kp, descriptors = self.sift_obj.detectAndCompute(window_gray_ubyte, None)
+        kp, descriptors = self.sift_obj.detectAndCompute(
+            window_gray_ubyte, None)
         del kp  # Free up memory
-
-
 
         # Is none if no descriptors are found, i.e. on 0 input range
         cluster_count = kmeans.n_clusters
