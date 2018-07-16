e532a504a9a3b4a113d51cc10e57c82c68a7ddec
Bouwe Andela <b.andela@esciencecenter.nl>
2018-07-16 15:42:22 +0200

    Add simple save function for feature images

--- satsense/__init__.py
+++ satsense/__init__.py
@@ -1,8 +1,15 @@
-from .bands import RGB, PLEIADES, WORLDVIEW2, WORLDVIEW3, QUICKBIRD, MONOCHROME
-from .extract import extract_features
+from .bands import MONOCHROME, PLEIADES, QUICKBIRD, RGB, WORLDVIEW2, WORLDVIEW3
+from .extract import extract_features, save_features
 from .image import SatelliteImage
 
 __all__ = [
-    'RGB', 'PLEIADES', 'WORLDVIEW2', 'WORLDVIEW3', 'QUICKBIRD', 'MONOCHROME',
-    "extract_features", "SatelliteImage"
+    'RGB',
+    'PLEIADES',
+    'WORLDVIEW2',
+    'WORLDVIEW3',
+    'QUICKBIRD',
+    'MONOCHROME',
+    'extract_features',
+    'save_features',
+    'SatelliteImage',
 ]
--- satsense/extract.py
+++ satsense/extract.py
@@ -1,8 +1,9 @@
 import numpy as np
-from six import iteritems
+from netCDF4 import Dataset
 
 
 def extract_features(features, generator):
+    """Compute features."""
     shape = generator.shape
 
     total_length = features.index_size
@@ -11,12 +12,23 @@ def extract_features(features, generator):
     print("Feature vector:")
     print(feature_vector.shape)
 
-    for feature in features.items.values():
-        if hasattr(feature, 'initialize'):
-            feature.initialize(generator.image)
-
     for cell in generator:
-        for name, feature in iteritems(features.items):
+        for feature in features.items.values():
             feature_vector[cell.x, cell.y, feature.indices] = feature(cell)
 
     return feature_vector
+
+
+def save_features(features, feature_vector, filename_prefix=''):
+    """Save computed features."""
+    for name, feature in features.items.items():
+        filename = filename_prefix + name + '.nc'
+        data = feature_vector[:, :, feature.indices]
+        with Dataset(filename, 'w') as dataset:
+            size_y, size_x, size_feature = data.shape
+            dataset.createDimension('y', size_y)
+            dataset.createDimension('x', size_x)
+            dataset.createDimension('feature', size_feature)
+            variable = dataset.createVariable(
+                name, 'f4', dimensions=('y', 'x', 'feature'))
+            variable[:] = data
