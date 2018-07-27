Fix parallel feature computation

satsense/extract.py
@@ -1,5 +1,5 @@
+"""Module for computing features."""
 import logging
-import math
 from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 from os import cpu_count
@@ -7,88 +7,20 @@ from os import cpu_count
 import numpy as np
 from netCDF4 import Dataset
 
-from .generators import CellGenerator
-
 logger = logging.getLogger(__name__)
 
 
-def _get_multiple(value, base):
-    """Get the nearest multiple of `base` that is larger than `value`."""
-    multiple = 0
-    while value > multiple * base:
-        multiple += 1
-    return multiple
-
-
-def extract_features_parallel(features,
-                              image,
-                              cell_size,
-                              max_jobs=cpu_count()):
+def extract_features_parallel(features, generator, n_jobs=cpu_count()):
     """Extract features in parallel."""
-    logger.debug("Extracting features using at most %s processes", max_jobs)
+    logger.debug("Extracting features using at most %s processes", n_jobs)
 
-    img_size = image.shape[0]
-    logger.debug("Image cells %s", math.ceil(img_size / cell_size[0]))
+    # Specify jobs
+    generators = generator.split(n_jobs=n_jobs, features=features)
 
-    # Compute number of cells in a job
-    job_cells = _get_multiple(img_size // max_jobs + 1, cell_size[0])
-    logger.debug('Job size: %s cells', job_cells)
-
-    # Compute number of cells needed as buffer
-    buffer_size = max(w[0] for f in features.items.values() for w in f.windows)
-    buffer_cells = _get_multiple(buffer_size, cell_size[0])
-    logger.debug('Buffer size: %s cells', buffer_cells)
-
-    # Select the smallest possible set of images to pickle
-    itypes = {f.base_image for f in features.items.values()}
-    selected_images = set()
-    # Images are derived in the following order, select only one
-    order = ('raw', 'normalized', 'rgb', 'grayscale', 'gray_ubyte')
-    for itype in order:
-        if itype in itypes:
-            selected_images.add(itype)
-            break
-    # Add images types that must be computed on the entire image
-    for itype in ('canny_edge', 'texton_descriptors'):
-        if itype in itypes:
-            getattr(image, itype)
-            selected_images.add(itype)
-    logger.debug("Images selected for job %s", selected_images)
-
-    # Create jobs
-    generators = []
-    n_jobs = 0
-    end = 0
-    while end < img_size:
-        start = max(0, n_jobs * (job_cells - buffer_cells) * cell_size[0])
-        end = min(img_size,
-                  (n_jobs + 1) * (job_cells + buffer_cells) * cell_size[0])
-        logger.debug("Creating job for x_range %s, %s", start, end)
-        image_chunk = image.shallow_copy_range(
-            x_range=slice(start, end), y_range=slice(None), pad=False)
-        image_chunk.collapse(selected_images)
-        generator = CellGenerator(image_chunk, cell_size)
-        generators.append(generator)
-        n_jobs += 1
-
-    logger.debug("Will use %s processes to extract features", n_jobs)
-
+    # Execute jobs
     with ProcessPoolExecutor() as executor:
-        # Submit jobs
         extract = partial(extract_features, features)
-        result = executor.map(extract, generators)
-
-        # Compute and store results
-        # TODO: change generator so the spurious cells are not computed
-        slices = [slice(buffer_cells, -buffer_cells) for _ in range(n_jobs)]
-        if n_jobs > 1:
-            slices[0] = slice(None, -buffer_cells)
-            slices[-1] = slice(buffer_cells, None)
-        else:
-            slices[0] = slice(None)
-
-        feature_vector = np.vstack(
-            chunk[s] for chunk, s in zip(result, slices))
+        feature_vector = np.vstack(executor.map(extract, generators))
 
     logger.debug("Done extracting features. Feature vector shape %s",
                  feature_vector.shape)
@@ -98,7 +30,8 @@ def extract_features_parallel(features,
 def extract_features(features, generator):
     """Compute features."""
     shape = (generator.shape[0], generator.shape[1], features.index_size)
-    feature_vector = np.zeros(shape, dtype=np.float32)
+    feature_vector = np.zeros(
+        (shape[0] * shape[1], shape[2]), dtype=np.float32)
     logger.debug("Feature vector shape %s", shape)
 
     # Pre compute images
@@ -112,7 +45,8 @@ def extract_features(features, generator):
         if i % (size // 10 or 1) == 0:
             logger.debug("%s%% ready", 100 * i // size)
         for feature in features.items.values():
-            feature_vector[cell.x, cell.y, feature.indices] = feature(cell)
+            feature_vector[i, feature.indices] = feature(cell)
+    feature_vector.shape = shape
 
     return feature_vector
 

test/test_extract.py
@@ -12,7 +12,7 @@ from satsense.generators import CellGenerator
 def image():
     """Create a test SatelliteImage instance."""
     bands = QUICKBIRD
-    shape = (100, 100, len(bands))
+    shape = (95, 50, len(bands))
     data = np.array(range(np.prod(shape)), dtype=np.float32)
     data.shape = shape
     return SatelliteImage(data, bands)
@@ -30,23 +30,9 @@ def load_image():
     return image
 
 
-def test_generator(image):
-
-    generator = CellGenerator(image, (25, 25))
-    assert len(generator) == len(tuple(generator))
-
-
-def test_padding(image):
-
-    generator = CellGenerator(image, (25, 25), length=(2, 180))
-    for cell in generator:
-        assert cell.shape == (25, 25)
-        assert cell.super_cell((100, 100)).shape == (100, 100)
-
-
 def test_extract_features(image):
 
-    generator = CellGenerator(image, (25, 25), length=(5, 10))
+    generator = CellGenerator(image, (10, 10), length=(10, 5))
 
     features = FeatureSet()
     features.add(Pantex(windows=((25, 25), (50, 50), (100, 100))))
@@ -56,17 +42,19 @@ def test_extract_features(image):
     assert results.any()
 
 
-def test_extract_features_parallel(image):
+@pytest.mark.parametrize("n_jobs", [1, 2, 3, 4, 5, 10])
+def test_extract_features_parallel(image, n_jobs):
     """Test that parallel extraction produces identical results."""
     cell_size = (10, 10)
     windows = [(25, 25), (50, 50)]
 
     features = FeatureSet()
     features.add(HistogramOfGradients(windows=windows))
-    print("Computing features in parallel")
-    results = extract_features_parallel(features, image, cell_size)
     print("Computing reference features")
     generator = CellGenerator(image, cell_size)
     reference = extract_features(features, generator)
+    print("Computing features in parallel")
+    generator = CellGenerator(image, cell_size)
+    results = extract_features_parallel(features, generator, n_jobs)
 
     np.testing.assert_array_almost_equal_nulp(results, reference)