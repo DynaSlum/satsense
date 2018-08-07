af6cd5554e657672ff944f71e9b293fd945ba541
Bouwe Andela <b.andela@esciencecenter.nl>
2018-08-07 14:16:47 +0200

    Add function get_ndxi_mask

--- satsense/util/mask.py
+++ satsense/util/mask.py
@@ -3,6 +3,10 @@ Methods for loading and saving mask images
 """
 import rasterio
 from scipy.misc import imread
+from skimage.filters import threshold_otsu
+
+from ..extract import extract_features_parallel
+from ..features import FeatureSet, NirNDVI
 
 
 def save_mask2file(mask, fullfname):
@@ -27,3 +31,15 @@ def load_mask_from_file(fullfname):
     mask = imread(fullfname)
 
     return mask
+
+
+def get_ndxi_mask(generator, feature=NirNDVI):
+    """Compute a mask based on an NDXI feature"""
+    features = FeatureSet()
+    windows = ((generator.x_size, generator.y_size), )
+    features.add(feature(windows=windows))
+
+    values = extract_features_parallel(features, generator)
+    values.shape = (values.shape[0], values.shape[1])
+
+    return values > threshold_otsu(values)
