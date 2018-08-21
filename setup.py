94cd45caa3104fec6b78d66401c877b7a60511c4
Bouwe Andela <b.andela@esciencecenter.nl>
2018-08-21 17:45:53 +0200

    Unpin packages and update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+"""Satsense package."""
 from setuptools import find_packages, setup
 
 with open('README.md') as readme:
@@ -12,26 +13,49 @@ setup(
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     install_requires=[
-        'gdal==2.1.3',
-        'descartes==1.1.0',
-        'fiona==1.7.8',
-        'numpy==1.12.1',
+        'descartes',
+        'fiona',
+        'gdal',
+        'netCDF4',
         'numba',
+        'numpy',
         'opencv-contrib-python-headless',
-        'shapely==1.5.17',
+        'rasterio',
+        'scikit-image',
+        'scikit-learn',
+        'scipy',
+        'shapely',
     ],
     extras_require={
-        'test': ['pytest', 'pytest-flake8', 'pytest-cov'],
+        'test': [
+            'pytest',
+            'pytest-cov',
+            'pytest-flake8',
+        ],
+        'dev': [
+            'isort',
+            'prospector[with_pyroma]',
+            'yamllint',
+            'yapf',
+        ],
+        'notebooks': [
+            'jupyter',
+            'matplotlib',
+            'nblint',
+        ],
     },
     author_email='b.weel@esiencecenter.nl',
     description=('Library for multispectral remote imaging.'),
     long_description=README,
     packages=find_packages(),
     include_package_data=True,
+    zip_safe=False,
     platforms='any',
     classifiers=[
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Development Status :: 3 - Alpha',
         'Natural Language :: English',
         'Intended Audience :: Developers',
