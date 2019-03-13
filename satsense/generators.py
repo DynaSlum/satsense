3b4febc61391f3e1adec560d1fd55d76b2d31856
Berend Weel <b.weel@esciencecenter.nl>
2019-03-13 14:20:42 +0100

    Fixing flake8 checks

--- satsense/generators.py
+++ satsense/generators.py
@@ -25,8 +25,8 @@ class BalancedGenerator():
             Satellite image
         masks: 1-D array-like
             List of masks, one for each class, to use for generating patches
-            A mask should have a positive value for the array positions that are
-            included in the class
+            A mask should have a positive value for the array positions that
+            are included in the class
         p: 1-D array-like, optional
             The probabilities associated with each entry in masks.
             If not given the sample assumes a uniform distribution
@@ -124,13 +124,9 @@ class BalancedGenerator():
         chunk_size = math.ceil(self.shape[0] / n_chunks)
         for job in range(n_chunks):
             row_offset = self.offset[0] + job * chunk_size[0]
-            #col_offset = self.offset[1] + job * chunk_size[1]
             row_length = min(chunk_size[0], self.shape[0] - row_offset)
-            #col_length = min(chunk_size[1], self.shape[1] - col_offset)
             if row_length <= 0:
                 break
-            #if col_length <= 0:
-            #    break
 
             yield BalancedGenerator(
                 image=self.image,
